78e3737c5575dd80b01cb5662350de225821aab1
josnielfermin <jfermin.wx@gmail.com>
2021-04-07 21:37:31 -0400

    primera parte lista

--- client/src/pages/Test.vue
+++ client/src/pages/Test.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
-    <div class="text-h3 col-10 row justify-center text-primary">Nomenclatura 1</div>
-    <q-card class="row justify-start bg-blue-2 no-wrap">
+    <div class="text-h3 col-10 row justify-center q-my-sm text-primary">Nomenclatura 1</div>
+    <q-card class="row justify-start bg-blue-2">
       <div class="column q-ma-md">
         <div class="text-h6 text-primary">Nueva Pregunta</div>
         <div class="row">
@@ -14,6 +14,23 @@
       <q-dialog v-model="newQ">
         <quest @question="toList"/>
       </q-dialog>
+    </div>
+    <div class="row justify-center">
+      <div class="col col-xs-12 col-sm-8 col-md-7 col-lg-5 col-xl-4 q-mx-md q-my-sm no-wrap">
+        <div class="column dimension no-wrap">
+          <q-card class="q-px-xl q-pt-md q-pb-lg q-ma-lg" v-for="(qt, index) in questions" :key="index">
+            <q-card class="bg-blue-2 q-pa-sm q-mb-md">
+              <div class="text-h6 q-ml-xs q-mb-sm">{{index + 1}} - {{qt.title}}</div>
+            </q-card>
+            <div class="column q-pl-lg">
+              <div class="text-subtitle1 q-mb-sm">A) {{qt.optionA}}</div>
+              <div class="text-subtitle1 q-mb-sm">B) {{qt.optionB}}</div>
+              <div class="text-subtitle1 q-mb-sm">C) {{qt.optionC}}</div>
+              <div class="text-subtitle1 q-mb-sm">D) {{qt.optionD}}</div>
+            </div>
+          </q-card>
+        </div>
+      </div>
     </div>
   </div>
 </template>
@@ -29,8 +46,17 @@ export default {
   },
   methods: {
     toList (quest) {
-      this.questions.push(quest)
+      if (quest) {
+        this.questions.push(quest)
+        this.newQ = false
+      }
     }
   }
 }
 </script>
+
+<style scoped lang="scss">
+.dimension {
+  min-width: 400px;
+}
+</style>
